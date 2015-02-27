a2d74c28ddd7a505790457eb50a35992908f3d05
Michael Sawyer <michaelsawyer92@gmail.com>
2015-02-26 19:32:04 -0800

    Adds a few more seed families.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 Rsvp.create!([
   {family: "Sawyer", coming: true, song: "Uptown Funk", message: "You guys are awesome!", count: 4},
   {family: "Trump", coming: false, song: "", message: "Sorry we can't make it :(", count: 0},
-  {family: "Hancock", coming: false, song: "National Anthem", message: "'Murica!", count: nil},
+  {family: "Hancock", coming: false, song: "National Anthem", message: "'Murica!", count: 0},
+  {family: "Jingle-Heimerschmidts", coming: true, song: "John Jacob Jingleheimer Schmidt", message: "Your name is my name too!", count: 3},
+  {family: "Joe", coming: false, song: nil, message: nil, count: 0}
 ])
